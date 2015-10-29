d86222bb6085e0ac3900ca77571d8055e3591a3c
kalpaca <swxyzabc@gmail.com>
2015-10-28 21:36:47 -0400

    tweak order of scripts

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,11 +6,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('css/eppo.css') }}" rel="stylesheet" type="text/css" />
         <!-- ES5, Html5 shim and Respond.js IE8 support of Html5 elements and media queries -->
         <script src="{{ asset('js/Modernizr.mq.min.js') }}"></script>
         <script>
             yepnope({
                 test: Modernizr.mq('only all'),
+                both: ["{{ asset('bower_components/jquery/dist/jquery.min.js')}}",
+                    "{{ asset('js/bootstrap.min.js') }}"
+                    ],
                 nope: [
                     "{{ asset('bower_components/es5-shim/es5-shim.min.js') }}",
                     "{{ asset('bower_components/es5-shim/es5-sham.min.js') }}",
@@ -18,15 +22,11 @@
                     "{{ asset('bower_components/html5shiv/dist/html5shiv.js') }}",
                     "{{ asset('bower_components/respond/dest/respond.min.js') }}",
                     "{{ asset('js/selectivizr.min.js') }}"             
-                    ],
-                //yep: ["{{ asset('css/bootstrap.paper.min.css') }}"],
-                both: ["{{ asset('bower_components/jquery/dist/jquery.min.js')}}",
-                    "{{ asset('js/bootstrap.min.js') }}"
                     ]
             });
         </script>
         
-        <link href="{{ asset('css/eppo.css') }}" rel="stylesheet" type="text/css" />
+        
     </head>
     <body>
         @include('partials.navbar')
