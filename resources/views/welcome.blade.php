798c6193800a58f8234495cf5e5c2eb982eba8a3
kalpaca <swxyzabc@gmail.com>
2015-11-03 14:15:18 -0500

    update to do list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,9 @@
 <li>Use Case: select patient</li>
 <li><a href="{{route('ppos.explore')}}">Use Case: select ppo (done) </a></li>
 <li><a href="{{route('prescriptions.create', ['ppoId'=>1,'diagnosisId'=>1])}}">Use Case: fill ppo (almost done)</a></li>
-<li><a href="{{route('prescriptions.create', ['ppoId'=>1,'diagnosisId'=>1])}}">Use Case: validate prescription input</a></li>
+<li><a href="{{route('prescriptions.create', ['ppoId'=>1,'diagnosisId'=>1])}}">Use Case: validate prescription input (in progress)</a></li>
 <li><a href="{{route('prescriptions.create', ['ppoId'=>1,'diagnosisId'=>1])}}">Use Case: save prescription (done)</a></li>
-<li>Use Case: view prescription</li>
+<li><a href="{{route('prescriptions.show', ['id'=>19])}}">Use Case: view prescription</a></li>
 <li>Use Case: print prescription</li>
 <li>Use Case: login and signup</li>
 </ul>
