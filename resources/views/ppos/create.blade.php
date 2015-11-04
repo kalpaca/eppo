cc7beb89f5f37a2d604ad48c60856fbd54cdaab0
kalpaca <swxyzabc@gmail.com>
2015-11-04 09:21:24 -0500

    fix letter case for linux server

--- resources/views/ppos/create.blade.php
+++ resources/views/ppos/create.blade.php
@@ -2,7 +2,7 @@
 @section('title','ppo')
 @section('panelHeading','Add a New PPO')
 @section('panelBody')
-{!! Form::model(new eppo\ppo, [
+{!! Form::model(new eppo\Ppo, [
     'route'=>'ppos.store',
     'class'=>'col-md-6'])
     !!}
