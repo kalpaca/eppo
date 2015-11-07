6185f01d858a63d740170f7b202f77e756942a54
kalpaca <swxyzabc@gmail.com>
2015-11-06 22:20:58 -0500

    updated description

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,19 +5,19 @@
   <div class="container">
     <h1>ePPO</h1>
     <p>Pre-Printed Orders for Take-Home Chemotherapy</p>
-    <p>Developed by Steven Song Wang, powered by Laravel, Bootstrap and JQuery frameworks</p>
+    <p>Developed by Steven Song Wang</p><p>Powered by Laravel, Bootstrap and JQuery frameworks</p>
     <a href="https://github.com/kalpaca/eppo" class="btn btn-primary">GitHub</a></p>
   </div>
 </div>
 <div class="container">
     <div class="col-md-12">
-        <h3>Summary</h3>
+        <h3>Description</h3>
 
-        <p>Health information administrators can use this tool to build a "combo" of medications
-         based on <a href="https://www.cancercare.on.ca/cms/one.aspx?portalId=1377&pageId=322076" target="_blank">CancerCare Ontario PrePrinted Orders (PPOs)</a>, which allows end users like
+        <p>"<a href="https://www.cancercare.on.ca/toolbox/drugformulary/pre_printed_orders/" target="_blank">CancerCare Ontario PrePrinted Orders (PPOs)</a> are protocol-specific forms that simplify and standardize the prescribing process. CancerCare Ontario pre-printed order forms for take-home chemotherapy prescriptions may reduce dosing, transcription and omission errors related to handwritten and verbal take-home chemotherapy orders. "</p>
+        <p>Health information administrators can use this tool to build such PPOs, which allow end users like
          Oncologists/Hematologists and Nurse Practitioners to make chemotherapy regimen prescriptions
-         and customize the combo of medications based on actual situation, for the patient to
-         take to a pharmacy to dispense medications.</p>
+         and customize the combo of medications based on the actual situation, for the patient to
+         take to a pharmacy to dispense medications. Also, This tool also simplify the process of choosing a PPO based on  diagnoses.</p>
         <hr>
 
         <h3>License</h3>
