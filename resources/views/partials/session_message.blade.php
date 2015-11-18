7d321847395d0641ecfccce419d061f82191d006
kalpaca <swxyzabc@gmail.com>
2015-11-18 16:06:37 -0500

    add message pattern

--- resources/views/partials/session_message.blade.php
+++ resources/views/partials/session_message.blade.php
@@ -3,6 +3,11 @@
         <p>{{ Session::get('success-message') }}</p>
     </div>
 @endif
+@if (Session::has('message'))
+    <div class="alert alert-default">
+        <p>{{ Session::get('message') }}</p>
+    </div>
+@endif
 @if (Session::has('warning-message'))
     <div class="alert alert-warning">
         <p>{{ Session::get('warning-message') }}</p>
