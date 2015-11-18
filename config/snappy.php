cb3c4891658b14e192cf32d824d6701e9291e9b6
kalpaca <swxyzabc@gmail.com>
2015-11-18 16:05:12 -0500

    add windows binary path as comment

--- config/snappy.php
+++ config/snappy.php
@@ -2,7 +2,10 @@
 return array(
     'pdf' => array(
         'enabled' => true,
+        //linux 64bit server
         'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        //windows 64bit server
+        //'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf.exe'),
         'timeout' => false,
         'options' => array(),
     )
