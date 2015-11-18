94198d8a2aced31dcea9158fcf66df53e2643a4d
kalpaca <swxyzabc@gmail.com>
2015-11-18 15:24:19 -0500

    change wkhtmltopdf bin path

--- config/snappy.php
+++ config/snappy.php
@@ -2,7 +2,7 @@
 return array(
     'pdf' => array(
         'enabled' => true,
-        'binary' => base_path('/vendor/bin/wkhtmltopdf-i386.bat'),
+        'binary' => base_path('/vendor/bin/wkhtmltopdf-i386'),
         'timeout' => false,
         'options' => array(),
     )
