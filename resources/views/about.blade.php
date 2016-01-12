257b5076ac2984b6eb53a7a15e450ca847dd97d7
SONG WANG <swangxyz@users.noreply.github.com>
2016-01-11 21:19:01 -0500

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,7 @@
     <h1>ePPO</h1>
     <p>Pre-Printed Orders for Take-Home Chemotherapy</p>
     <p>Developed by Steven Song Wang</p><p>Powered by Laravel, Bootstrap and JQuery frameworks</p>
-    <a href="https://github.com/kalpaca/eppo" class="btn btn-primary">GitHub</a></p>
+    <a href="https://github.com/swangxyz/eppo" class="btn btn-primary">GitHub</a></p>
   </div>
 </div>
 <div class="container">
@@ -41,20 +41,7 @@
         <p>Please refer to the CancerCare Ontario and related authorities for additional information.</p>
         <hr>
 
-        <h3>To Do List</h3>
-        <ul class="list-group">
-        <li class="list-group-item"><a href="{{route('ppos.create')}}">Use Case: build a ppo (done)</a></li>
-        <li class="list-group-item"><a href="{{route('ppoitems.create',['id'=>1])}}">Use Case: add dosing schedule to a ppo (done)</a></li>
-        <li class="list-group-item"><a href="{{route('ppos.show',['id'=>1])}}">Use Case: view a ppo (done)</a></li>
-        <li class="list-group-item"><a href="{{route('patients.show',['id'=>2])}}">Use Case: select a patient (done)</a></li>
-        <li class="list-group-item"><a href="{{route('ppos.explore',['id'=>1])}}">Use Case: select a ppo (done) </a></li>
-        <li class="list-group-item"><a href="{{route('prescriptions.create', ['ppoId'=>1,'diagnosisId'=>1,'patientId'=>1])}}">Use Case: fill a ppo (almost done)</a></li>
-        <li class="list-group-item">Use Case: validate a prescription input (in progress)</li>
-        <li class="list-group-item">Use Case: save a prescription (done)</li>
-        <li class="list-group-item"><a href="{{route('prescriptions.show', ['id'=>26])}}">Use Case: view a prescription (done)</a></li>
-        <li class="list-group-item">Use Case: print prescription (done, use wkhtmltopdf, default set for linux amd64 server)</li>
-        <li class="list-group-item">Use Case: login and signup (done)</li>
-        </ul>
+
     </div>
 </div>
 @endsection
